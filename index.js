9597ed694ac24d269d4fdf27aee28fd8f2d2655d
Vincent <1155062425@link.cuhk.edu.hk>
2018-03-18 19:01:21 +0800

    updated all, need node js to run

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let express = require('express');
 let session = require('express-session');
 let app = express();
 
-require('./js/db.js');    // Set up DB
+//require('./js/db.js');    // Set up DB
 
 app.use( session({
     secret: 'mysecretkey',
@@ -14,7 +14,6 @@ app.use( session({
 }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
-
 // Setup the routing rules in ./js/routes
 app.use(require('./js/routes.js'));
 
---
+++ js/routes.js
@@ -0,0 +1,45 @@
+// This file contains the routing rules for the app
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({extended:false});
+let jsonParser = bodyParser.json({});
+
+// For constructing query string from object properties
+const Query = require('query-string');
+
+// For convenience, just name the router 'app'
+let app = require('express').Router();
+
+//let model = require('./model.js');
+
+module.exports = app;
+
+// Use a separate router for ajax request
+//app.use('/ajax', require('./ajaxRoutes.js'));
+var path = require('path');
+app.get('/', (req, res) => {
+	res.sendFile('landingLogin.html', { root: path.join(__dirname, '../views') });
+});
+
+app.get('/profile', (req, res) => {
+	res.sendFile('ProfilePage.html', { root: path.join(__dirname, '../views') });
+});
+
+app.get('/schedule', (req, res) => {
+	res.sendFile('schedule.html', { root: path.join(__dirname, '../views') });
+});
+
+app.get('/report', (req, res) => {
+	res.sendFile('report.html', { root: path.join(__dirname, '../views') });
+});
+
+app.post('/login', urlencodedParser, async (req, res) => {
+  res.send("Not implement yet");
+});
+
+app.post('/report', urlencodedParser, async (req, res) => {
+  res.send("Not implement yet");
+});
+// CSS files, images, client-side JS files should be in ./public
+app.use(express.static('public'));
