2449add23dda0045ccaea3963472248ae0038d74
Alex Vong <alexvong1995@gmail.com>
2018-03-31 14:23:35 +0800

    bootstrap: No need to install node modules anymore.

--- bootstrap.py
+++ bootstrap.py
@@ -1,18 +1,8 @@
-from os import listdir
-from re import match
 from shutil import which
 from subprocess import call
-
-def npm_install(*args):
-    return call([which("npm"), "install"] + list(args))
 
 def node(*args):
     return call([which("node")] + list(args))
 
-node_modules = list(filter(lambda str: match(r"^[^\\._]", str),
-                           listdir("node_modules"))) + ["bcryptjs"]
-
-npm_install()
-npm_install(*node_modules)
 node("initdb.js")
 node("index.js")
