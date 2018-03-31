e846504b563937fbe8d30a1dcd566a36d3c81e6d
Alex Vong <alexvong1995@gmail.com>
2018-03-31 21:14:09 +0800

    bootstrap: Use single quote.

--- bootstrap.py
+++ bootstrap.py
@@ -2,7 +2,7 @@ from shutil import which
 from subprocess import call
 
 def node(*args):
-    return call([which("node")] + list(args))
+    return call([which('node')] + list(args))
 
-node("initdb.js")
-node("index.js")
+node('initdb.js')
+node('index.js')
