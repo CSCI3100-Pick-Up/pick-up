aac7edecadc3af77cd989593856b59ce622fbc81
Alex Vong <alexvong1995@gmail.com>
2018-04-02 13:01:55 +0800

    Scan: Mostly completed.

--- scan.py
+++ scan.py
@@ -1,20 +1,25 @@
 from datetime import datetime
 from interval import interval
+from itertools import chain, groupby
 from pymongo import MongoClient
 from sys import argv
 from time import mktime
-from itertools import chain, groupby
 
-union = interval.union
 concat = chain.from_iterable
+join = str.join
+strptime = datetime.strptime
+fromtimestamp = datetime.fromtimestamp
 
 client = MongoClient()
 db = client['csci3100PickUp']
 scheds = db['schedules']
 users = db['users']
 
+def delete_key(key, dic):
+    return {k: dic[k] for k in dic if k != key}
+
 def parse_time(s):
-    return mktime(datetime.strptime(s, '%B %d, %Y %H:%M:%S').timetuple())
+    return mktime(strptime(s, '%B %d, %Y %H:%M:%S').timetuple())
 
 def email_to_oid(email):
     return users.find_one({'email': email})['_id']
@@ -37,103 +42,92 @@ def normalize_sched(sched):
 def normalize_scheds(scheds):
     return [normalize_sched(sched) for sched in scheds]
 
-def filter_keys(dic, keys):
-    return {k: dic[k] for k in dic if k not in keys}
-
-def sched_ls_to_dict(scheds):
+def scheds_to_dict(scheds):
     sched_act = lambda sched: sched['activity']
     sorted_scheds = sorted(scheds, key=sched_act)
-    return {act: [filter_keys(sched, {'activity'}) for sched in grp]
+    return {act: [sched for sched in grp]
             for act, grp in groupby(sorted_scheds, sched_act)}
 
-my_oid = email_to_oid('john@example.com') #argv[1]
+def intersect(scheds, itvs):
+    return ({'interval': sched['interval'] & itv,
+             **delete_key('interval', sched)}
+            for sched in scheds
+            for itv in itvs)
 
-my_scheds = normalize_scheds(scheds.find({'owner': my_oid}))
+def normalize_html(html):
+    return html.replace(' ', '&nbsp;').replace('\n', '<br>')
 
-my_acts = {sched['activity'] for sched in my_scheds}
+def format_time(time):
+    fmt = normalize_html('%B %d, %Y %H:%M:%S')
+    return fromtimestamp(time).strftime(fmt)
 
-my_scheds_2 = sched_ls_to_dict(my_scheds)
+def format_itv(itv):
+    (a, b), = itv
+    fmt = '''from  {start}
+until  {end}'''
+    return normalize_html(fmt).format(start=format_time(a), end=format_time(b))
 
-other_scheds = concat(scheds.find({'content': act}) for act in my_acts)
+def format_match(match):
+    return '''
+<tr>
+  <th>
+    <img class="img-fluid"
+         src={image}
+         style="max-width: 50%;">
+  </th>
+  <td>{activity}</td>
+  <td>{username}</td>
+  <td>{interval}</td>
+  <td>
+    <button class="btn btn-success"
+            type="button">
+      CONFIRM
+    </button>
+  </td>
+</tr>
+'''.format(activity=match['activity'],
+           username=match['username'],
+           interval=format_itv(match['interval']),
+           image='"../icons/whois-icon.png"')
 
-other_scheds_2 = normalize_scheds(sched
-                                  for sched in other_scheds
-                                  if sched['owner'] != my_oid)
+def format_matches(matches):
+    return '<tbody>' + join('', [format_match(match)
+                                 for match in matches]) + '</tbody>'
 
-#other_scheds_3 = [sched
-#                  for sched in other_scheds_2 if match_time()]
+def format_field(field):
+    return '<th scope="col">{field}</th>'.format(field=normalize_html(field))
 
-#other_scehds_3 = (sched
-#                  for sched in other_scheds_2
-#                  if sched['owner'] != my_oid)
+def format_feilds(fields):
+    return '<thead><tr>' + join('', [format_field(field)
+                                     for field in fields]) + '</tr></thead>'
 
-#other_scheds_2 = [sched for sched in other_scheds
-#                  if my_scheds_2[sched['activity']]]
-
-#other_scheds = concat(filter(lambda sched: sched_time(sched) & my_scheds_2[act],
-#                             ))
-#                      for act in my_acts)
-#other_scheds_2 = scheds_to_tups(other_scheds)
-
-# TODO: generate table dynamically
-
-print('''
+def format_html(matches):
+    fields = ['', 'Activity', 'Name', 'Matching time slot']
+    text = '' if matches else 'No matches found.'
+    html = '''
 <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">Activity</th>
-      <th scope="col">Name</th>
-      <th scope="col">Matching time slot</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th>
-        <img class="img-fluid"
-             src="../icons/whois-icon.png"
-             style="max-width: 50%;">
-      </th>
-      <td>Football</td>
-      <td>John</td>
-      <td>10:30 am - 11:30 am</td>
-      <td>
-        <button class="btn btn-success"
-                type="button">
-          CONFIRM
-        </button>
-      </td>
-    </tr>
-    <tr>
-      <th>
-        <img class="img-fluid"
-             src="../icons/whois-icon.png"
-             style="max-width: 50%;">
-      </th>
-      <td>Basketball</td>
-      <td>Marry</td>
-      <td>2:30 pm - 3:30 pm</td>
-      <td>
-        <button class="btn btn-success"
-                type="button">
-          CONFIRM
-        </button>
-      </td>
-    </tr>
-    <tr>
-      <th>
-        <img class="img-fluid"
-             src="../icons/whois-icon.png"
-             style="max-width: 50%;">
-      </th>
-      <td>Badminton</td>
-      <td>Peter</td>
-      <td>7:30 pm - 8:30 pm</td>
-      <td>
-        <button class="btn btn-success" type="button">
-          CONFIRM
-        </button>
-      </td>
-    </tr>
-  </tbody>
-</table>'''.replace('\n', ' '))
+  {fields}
+  {matches}
+</table>
+{text}
+'''.format(fields=format_feilds(fields),
+           matches=format_matches(matches),
+           text=normalize_html(text))
+    return html.replace('\n', ' ')
+
+_, my_email = argv
+my_sched_ls = normalize_scheds(scheds.find({'owner': email_to_oid(my_email)}))
+my_sched_dict = scheds_to_dict(my_sched_ls)
+
+sched_ls = normalize_scheds(concat(scheds.find({'content': act})
+                                   for act in my_sched_dict))
+sched_dict = scheds_to_dict(sched_ls)
+
+overlaps = concat(intersect(sched_dict[act],
+                            [sched['interval'] for sched in my_sched_dict[act]])
+                  for act in my_sched_dict)
+matches = [sched
+           for sched in overlaps
+           if sched['interval'] and sched['email'] != my_email]
+
+print(format_html(matches))
