db8a5417d2bbb50df2452d6887c3dd48c3d87719
Alex Vong <alexvong1995@gmail.com>
2018-04-03 18:28:48 +0800

    matches: Fix formatting.

--- js/matches.js
+++ js/matches.js
@@ -4,14 +4,15 @@ module.exports = app
 
 const title = 'PickUp - Scan Result'
 app.get('/matches',
-        (req,
-         res) => {if (!req.session.user)
-                  res.redirect('/')
-                  else py.run('py/scan.py',
-                              {args: [req.session.user]},
-                              (err,
-                               table) => {if (err)
-                                          console.log(err)
-                                          else res.render('matches.ejs',
-                                                          {title: title,
-                                                           table: table})})})
+
+        (req, res) =>
+        {if (!req.session.user)
+         res.redirect('/')
+         else py.run('py/scan.py',
+                     {args: [req.session.user]},
+
+                     (err, table) =>
+                     {if (err)
+                      console.log(err)
+                      else res.render('matches.ejs', {title: title,
+                                                      table: table})})})
