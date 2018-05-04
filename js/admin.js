192ae0e4bfe7bb8669824a78d00f04679f800a33
winsonwong1234 <1155063275@link.cuhk.edu.hk>
2018-05-04 20:01:17 +0800

    Added comment

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,19 @@
 // This file contains the schedule rules for the app
 
+/*
+PROGRAM ADMIN - Program to prepare data and redirect to admin page
+PROGRAMMER: Wong Muk Kit 1155063275@link.cuhk.edu.hk
+CALLING SEQUENCE: HTTP://LOCALHOST:8081/admin
+VERSON 1: written 3-4-2018
+PURPOSE: The admin can view all the user's information and report details from the database
+DATA STRUCTURE:
+  Variable req.session.user - STRING
+  Variable ReportData - OBJECT
+  Variable UserData - OBJECT
+
+ALGORITHM: It first check whether the connecting user is the admin. Then it will retrieve ReportData and UserData from the report database and user database respectively and return the results to the admin page for rendering
+*/
+
 let express = require('express');
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended:false});
